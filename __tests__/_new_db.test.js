12d08cf6c6edc6ee9140d67a84259dfaeb903c2a
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-14 22:05:26 +0100

    yea works... ~3s to create 2.5k databases >> FOLDERS [dir]...then similar time takes to remove those

--- __tests__/_new_db.test.js
+++ __tests__/_new_db.test.js
@@ -5,22 +5,50 @@ const testData = {
     name: "wee_db_11",
     pass: "123456789_V-core9"
   },
-  dbTestCount: 5,
+  dbTestCount: 2500,
 };
 
-  v_db.mk(testData.db.name , testData.db.pass);
 
-  for (let i = 1; i <= testData.dbTestCount; i++) {
-    v_db.mk(testData.db.name + "__" + i , testData.db.pass );
-  }
-
-
-  setTimeout(() => {
-    v_db.rm(testData.db.name);
+mkTestBases = async () => {
+  try {
+    const res = [ v_db.mk(testData.db.name, testData.db.pass) ];
 
     for (let i = 1; i <= testData.dbTestCount; i++) {
-      setTimeout(() => {
-        v_db.rm(testData.db.name + "__" + i);
-      }, 500 * i);
+      res.push(await v_db.mk( testData.db.name + "__" + i, testData.db.pass));
     }
-  }, 2000);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+rmTestBases = async () => {
+  try {
+    const res = [ await v_db.rm(testData.db.name)];
+
+    for (let i = 1; i <= testData.dbTestCount; i++) {
+      res.push( await v_db.rm(testData.db.name + "__" + i));
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+testIt = async () => {
+
+  console.log(`DB TEST COUNT : ${testData.dbTestCount}`);
+
+  console.time('GENERATE');
+  const res1 = await mkTestBases();
+  console.log(res1);
+  console.timeEnd('GENERATE');
+
+  console.time('REMOVE');
+  const res2 = await rmTestBases();
+  console.log(res2);
+  console.timeEnd('REMOVE');
+
+};
+
+testIt();
