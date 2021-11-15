0d9eeffaf263f3912e0fb8197cfd05c8c61d6029
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-15 10:16:30 +0100

    yea

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ const path = require('path');
 
 const v_db = {
   config: require('../v_config'),
-  options: null,
   newType: require(path.join(__dirname,'./_new_type')),
   delType: require(path.join(__dirname,'./_del_type')),
   listTypes: require(path.join(__dirname,'./_list_types')),
-  init() {
-    console.log("V_DB->INIT()");
-  }
 };
 
-v_db.init();
 
 
 module.exports = v_db;
