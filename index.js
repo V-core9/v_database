7bd33d9fec8c8f115eed573d8222863d142ac4d9
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-15 05:36:18 +0100

    yea clean

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+process.on('exit', () => {
+  console.log("EXITING >>>");
+});
+
+process.title = "v_db - V-core9_DataBase";
+console.log(`Process Title : ${process.title}`);
+
 const v_db = require('./src/');
 
 module.exports = v_db;
