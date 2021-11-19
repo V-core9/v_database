1ad67f633f64dee1977c9a7a0e59f7370a8ef0ea
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-19 20:11:51 +0100

    yea

--- _TRYOUTS_/__.worker-threads.tryout.js
+++ _TRYOUTS_/__.worker-threads.tryout.js
@@ -1,37 +1,34 @@
 const { Worker, isMainThread } = require('worker_threads');
 
 const v_app = {
-  _status: null,
-  $_status () {
-    return v_app._status;
-  },
-  config: {
-    maxCpuUsage: 50,
-    maxWorkerCount: null,
-  },
-  workers: [],
 
   init() {
-
     if (isMainThread) {
-      if (v_app._status === null ) {
-        v_app.config.maxWorkerCount = process.env.NUMBER_OF_PROCESSORS * (v_app.config.maxCpuUsage / 100) ;
+
+
+      v_app._status = null;
+      v_app.config = {
+        maxCpuUsage: 50,
+        maxWorkerCount: null,
+        cpuCount: Number(process.env.NUMBER_OF_PROCESSORS)
+      },
+        v_app.workers = [];
+
+      if (v_app._status === null) {
+        v_app.config.maxWorkerCount = process.env.NUMBER_OF_PROCESSORS * (v_app.config.maxCpuUsage / 100);
         console.log(`🔥 Maximum Number of Workers [ ${v_app.config.maxWorkerCount} ]`);
       }
-
-
-
-      // This re-loads the current file inside a Worker instance.
-      //new Worker(__filename);
-      //console.log(v_app);
+      for (let i = 1; i < v_app.config.maxWorkerCount; i++) {
+        // This re-loads the current file inside a Worker instance.
+        v_app.workers["v_worker#" + i] = { worker: new Worker(__filename), created_ts: Date.now() };
+      }
+      console.log(v_app);
     } else {
-      console.log('🏭 Worker Space  >> [ isMainThread :: '+ isMainThread +' ]');
-      //console.log(isMainThread);  // Prints 'false'.
+      console.log('🏭 Worker Space  >> [ isMainThread :: ' + isMainThread + ' ]');
     }
   }
 };
 
 
 v_app.init();
-
 
