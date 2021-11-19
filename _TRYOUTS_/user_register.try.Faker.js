a00f2b846c706210452fe89e52f0a031c3509572
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-19 11:57:56 +0100

    yea

--- _TRYOUTS_/user_register.try.Faker.js
+++ _TRYOUTS_/user_register.try.Faker.js
@@ -1,16 +1,9 @@
 const v_db = require("../index");
-const testData = require('./__test_data');
 const user = require("../input_templates/user");
-var v_to_sha256 = require('v_to_sha256');
 var faker = require('faker');
 
 test_it = async () => {
   await v_db.type.new('users');
-  await user.register(testData.demoUserBAD);
-  await user.register(testData.demoUserBAD2);
-  await user.register(testData.demoUserBAD3);
-  await user.register(testData.demoUser);
-
 
   for (let i = 0; i < 50; i++) {
     user.register({
@@ -19,9 +12,8 @@ test_it = async () => {
       password: "123456789",
       password_confirm: "123456789"
     });
-
-    console.log(v_to_sha256("123456789"));
   }
+
 };
 
 test_it();
--- input_templates/_username.js
+++ input_templates/_username.js
@@ -1,12 +1,18 @@
-module.exports =  (username) => {
-  var min = 6;
-  var max= 32;
-  var errorMsg ={
-    chars : { type: "ERROR", message: "🙋‍♂️ Username can only have letters, numbers, underscore and dot. [ " + username + " ]" },
-    min : { type: "ERROR", message: "🤯 Username minimum length is [ " + min + " ]" },
-    max : { type: "ERROR", message: "💥 Username maximum length is [ " + max + " ]" },
-  };
-  if (username.length < min ) return errorMsg.min;
-  if (username.length > max ) return errorMsg.max;
-  return !( /^[A-Za-z][A-Za-z0-9_.]{(min-1),(max-1)}$/.test(username)) ? true : errorMsg.chars;
+const _config = {
+  min: 6,
+  max: 32,
+  format: /^[A-Za-z][A-Za-z0-9_.]{(min-1),(max-1)}$/
+};
+
+var errorMsg = {
+  chars: { type: "ERROR", message: "🙋‍♂️ Username can only have letters, numbers, underscore and dot." },
+  min: { type: "ERROR", message: "🤯 Username minimum length is [ " + _config.min + " ]" },
+  max: { type: "ERROR", message: "💥 Username maximum length is [ " + _config.max + " ]" },
+};
+
+module.exports = (username) => {
+  if (username.length < _config.min) return errorMsg.min;
+  if (username.length > _config.max) return errorMsg.max;
+  if (_config.format.test(username) === true) return errorMsg.chars;
+  return true;
 };
