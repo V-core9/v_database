0456792c30f497693056cfc071af591c5a3f11ba
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-19 16:26:39 +0100

    yea

--- src/app/data_size.js
+++ src/app/data_size.js
@@ -3,43 +3,61 @@ const v_fs = require('v_file_system');
 const path = require('path');
 
 data_size = async () => {
+  console.log("🔀 Getting Data Size ");
+
   const helper = {
     sizes : {
       totalSize: 0,
+      totalCount: 0,
       types:[],
     },
     types: null,
     typeCount: null,
-    itemsCount: 0,
-    items: []
   };
-  helper.types = await v_db.type.view();
 
+  helper.types = await v_db.type.view();
   helper.typeCount = helper.types.length;
 
+
   for (let i = 0; i < helper.typeCount; i++) {
-    helper.items[helper.types[i]] = await v_db.item.view(helper.types[i]);
-    helper.itemsCount++;
+    helper.types[i] = {
+      type : helper.types[i],
+      items: [],
+    };
   }
-  //console.log(helper);
+
+  //console.log(helper );
+
+  for (let i = 0; i < helper.typeCount; i++) {
+    helper.types[i].items = await v_db.item.view(helper.types[i].type);
+  }
 
 
-  for (let i = 0; i < helper.itemsCount; i++) {
+  for (let i = 0; i < helper.typeCount; i++) {
     var typeSize = 0;
-    var innerCount = helper.items[helper.types[i]].length;
+    var innerCount = helper.types[i].items.length;
     //console.log("inner count: " +innerCount);
     for (let j = 0; j < innerCount; j++){
-      const itemPath = path.join(__dirname, '../../$_data/'+helper.types[i]+'/'+helper.items[helper.types[i]][j]);
+      const itemPath = path.join(__dirname, '../../$_data/'+helper.types[i].type+'/'+helper.types[i].items[j]);
       //console.log(itemPath);
       const stats = await v_fs.promise.statsFile(itemPath);
       typeSize += stats.size;
       //console.log(stats);
     }
-    helper.sizes.types.push({type: helper.types[i], size: typeSize});
+    helper.sizes.types.push({type: helper.types[i].type, count: innerCount, size: typeSize});
     helper.sizes.totalSize += typeSize;
+    helper.sizes.totalCount += innerCount;
   }
-  console.log(JSON.stringify(helper.sizes, true, 2));
 
+
+  helper.sizes.types.forEach(item => {
+    item.percent = Math.round((item.size/helper.sizes.totalSize)*100 *10)/10 +"%";
+    item.size = Math.trunc(v_fs.byteSizer.byteToMega(item.size) * 100)/100+"MB";
+  });
+  console.table(helper.sizes.types);
+  console.log("🔄 Total Data Disk Size : [ "+ Math.trunc(v_fs.byteSizer.byteToGiga(helper.sizes.totalSize)* 100)/100+"GB ]");
+  console.log("⏩ Total Items Count : [ "+ helper.sizes.totalCount + " ]");
+  console.log("🔂 Total Types Count : [ "+ helper.typeCount + " ]");
 };
 
 
