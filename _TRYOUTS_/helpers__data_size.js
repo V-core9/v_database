d88e433b211d6f1d1b447b6fd772041d3100cd8f
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-20 20:50:04 +0100

    yea small fixs for tryouts

---
+++ _TRYOUTS_/helpers__data_size.js
@@ -0,0 +1,6 @@
+
+
+const v_db = require('../src/index');
+
+v_db.helpers.data_size();
+
--- src/db_size-demo.js
+++
@@ -1,9 +0,0 @@
-
-
-const v_db = require('./index');
-
-const sizeDB = async () => {
-  console.log(await v_db.helpers.data_size());
-};
-
-sizeDB();
