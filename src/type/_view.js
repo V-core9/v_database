346486e84c99b1a91c570d9c6b1a08e53ff94290
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-20 18:51:56 +0100

    ea

--- src/type/_view.js
+++ src/type/_view.js
@@ -1,5 +1,5 @@
 const v_fs = require('v_file_system');
 
 module.exports = async () => {
-  return (await v_fs.promise.listDir(process.v.data_dir)) || false;
+  return await v_fs.promise.listDir(process.dataDir);
 };
