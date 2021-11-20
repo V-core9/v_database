3cce29cf1ae41744d7fea223d49c44cdacb47c38
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-20 12:20:09 +0100

    yea

--- src/config/v_config.js
+++ src/config/v_config.js
@@ -4,13 +4,13 @@ const npmInfoPath = path.join(__dirname, '../../package.json');
 
 const npmInfo = JSON.parse(v_fs.sync.read(npmInfoPath));
 
-//console.log(npmInfo._v_);
+console.log(npmInfo._v_);
 
 module.exports = {
   title: npmInfo._v_.name,
   mode: `dev`,
   consoleOutput: false,
-  dbName: `$_data`,
+  dbName: path.join(__dirname, `../../$_data`),
   dbMode: `perPost`,
   npmInfo: npmInfo
 };
