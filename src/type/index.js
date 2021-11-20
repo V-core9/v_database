c4fb105d21f39979edf192a33d7dfa34e4bdddd8
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-20 18:33:15 +0100

    yea

--- src/type/index.js
+++ src/type/index.js
@@ -3,6 +3,6 @@ module.exports = {
   new: require('./_new'),
   del: require('./_del'),
   view: async () => {
-    ohg yea... await v_fs.promise.listDir(process.v.data_dir) || false;
-},
+    return await v_fs.promise.listDir(process.v.data_dir) || false;
+  },
 };
