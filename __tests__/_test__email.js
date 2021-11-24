b4b184903a7d83c25d2747f814d42b3dd832706d
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-24 07:24:16 +0100

    yea more testing

--- __tests__/_test__email.js
+++ __tests__/_test__email.js
@@ -1,23 +1,124 @@
-const user_schema = require('./schema.js');
+const user_schema = require('../sys_module/user/schema.js');
 
-test_username = async () => {
-  console.log(await user_schema.email.validate('Demo'));
-  console.log(await user_schema.email.validate('DemoUser'));
-  console.log(await user_schema.email.validate('.'));
-  console.log(await user_schema.email.validate('..'));
-  console.log(await user_schema.email.validate('.._'));
-  console.log(await user_schema.email.validate('De_'));
-  console.log(await user_schema.email.validate('De/.mo'));
-  console.log(await user_schema.email.validate('De\lmo'));
-  console.log(await user_schema.email.validate('De\\mo'));
-  console.log(await user_schema.email.validate('Demo\\'));
-  console.log(await user_schema.email.validate('Demo_____1@mail.com'));
-  console.log(await user_schema.email.validate('Demo123455367345531231264362352341234123123123123'));
-  console.log(await user_schema.email.validate('Demo@@3123@mail.com'));
-  console.log(await user_schema.email.validate('Demo123123@mail.com'));
-  console.log(await user_schema.email.validate('XZCZXCSDAFSAFA@mail.com'));
-  console.log(await user_schema.email.validate('WEQ23652@mail.com'));
 
+const outString = (string) => {
+  return [
+    { 
+      "confirm": undefined, 
+      "input_value": string, 
+      "msg": user_schema.email.msg.error.format, 
+      "suggest": "Try removing special characters.", 
+      "type": "error" 
+    }
+  ];
 };
 
-test_username();
+
+const email_test_list = [
+  {
+    input: 'Demo',
+    output: outString
+  },
+  {
+    input: 'DemoUser',
+    output: outString
+  },
+  {
+    input: '.',
+    output: outString
+  },
+  {
+    input: '..',
+    output: outString
+  },
+  {
+    input: '.._',
+    output: outString
+  },
+  {
+    input: 'De_',
+    output: outString
+  },
+  {
+    input: 'De/.mo',
+    output: outString
+  },
+  {
+    input: 'De\lmo',
+    output: outString
+  },
+  {
+    input: 'De\\mo',
+    output: outString
+  },
+  {
+    input: 'Demo\\',
+    output: outString
+  },
+  {
+    input: 'Demo_____1@mail.com',
+    output: true
+  },
+  {
+    input: 'Demo123455367345531231264362352341234123123123123',
+    output: outString
+  },
+  {
+    input: 'Demo@@3123@mail.com',
+    output: outString
+  },
+  {
+    input: 'Demo123123@mail.com',
+    output: true
+  },
+  {
+    input: 'XZCZXCSDAFSAFA@mail.com',
+    output: true
+  },
+  {
+    input: 'WEQ23652@mail.com',
+    output: true
+  },
+  {
+    input: '@WEQ23652@mail.com',
+    output: outString
+  },
+  {
+    input: 'WEQ@23652@mail.com',
+    output: outString
+  },
+  {
+    input: 'WEQ23@@652@mail.com',
+    output: outString
+  },
+  {
+    input: 'WEQ23652+@mail.com',
+    output: true
+  },
+  {
+    input: 'WEQ2+3652@mail.com',
+    output: true
+  },
+  {
+    input: '@@+WEQ23652@mail.com',
+    output: outString
+  },
+  {
+    input: '+WEQ23652@mail.com',
+    output: true
+  },
+  {
+    input: 'shouldWork11@mail+com',
+    output: outString
+  }
+];
+
+
+email_test_list.forEach(item => {
+  test('User Schema : EMAIL  ?? [ ' + item.input + ' ]', async () => {
+    expect(await user_schema.email.validate(item.input)).toEqual((item.output === true) ? true : item.output(item.input));
+  });
+});
+
+
+
