8675c847d2991bf05c469425ab79542808b3d8d7
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-12 15:27:42 +0100

    fix for install config

--- src/config/_$.js
+++ src/config/_$.js
@@ -2,7 +2,7 @@
 
 const d_dir = '.v_database';
 const d_cfg = 'v_config';
-const cfg_dpath = process.env.home+'/'+d_dir+'/'+d_cfg;
+const cfg_dpath = process.env.home+'/'+d_dir;
 const cfg_fpath = process.env.home+'/'+d_dir+'/'+d_cfg;
 
 module.exports = {
