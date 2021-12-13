5d94b0d283afa23d6e90e0103993e80fb32e4d25
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-13 13:19:38 +0100

    yea fixes

--- src/item/_new.js
+++ src/item/_new.js
@@ -2,5 +2,5 @@ const v_fs = require('v_file_system');
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = async (type, content, id = null) => {
-  return await v_fs.write(`${process.v.data_dir}/${type}/${(id !== null) ? id : uuidv4()}.json`, JSON.stringify(content));
+  return await v_fs.write(`${process.v.data_dir}/${type}/${(id === null) ? uuidv4() : id}.json`, JSON.stringify(content));
 };
