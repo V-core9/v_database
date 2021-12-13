3162b56e98838afe720aef74628d7825aa8efbb8
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-13 13:12:25 +0100

    yea

--- src/item/_new.js
+++ src/item/_new.js
@@ -1,7 +1,6 @@
 const v_fs = require('v_file_system');
 const { v4: uuidv4 } = require('uuid');
 
-module.exports = async (type, content) => {
-  const id = (content.id !== undefined) ? content.id : uuidv4();
-  return await v_fs.write(`${process.v.data_dir}/${type}/${id}.json`, JSON.stringify(content));
+module.exports = async (type, content, id = null) => {
+  return await v_fs.write(`${process.v.data_dir}/${type}/${(id !== null) ? id : uuidv4()}.json`, JSON.stringify(content));
 };
