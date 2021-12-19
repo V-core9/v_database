9fc09c4e8d4e6139d789e31fdd98d6108cf6f742
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-19 14:07:05 +0100

    yea init

--- install.js
+++ install.js
@@ -1,5 +1,5 @@
 const v_db = require('.');
 
 (async () => {
-  if (await v_db.check_config_dir() === false || await v_db.check_config_file() === false ) await v_db.install();
+  if (await v_db.check_config_dir() === false || await v_db.check_config_file() === false ) await v_db.init();
 })();
