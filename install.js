ea5925c46f9c53323e45c3574d34b9b308be8892
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-19 14:01:19 +0100

    yea

--- install.js
+++ install.js
@@ -1,6 +1,4 @@
-const v_fs = require('v_file_system');
-const path = require('path');
-const v_db = require(path.join(__dirname, 'index'));
+const v_db = require('.');
 
 (async () => {
   if (await v_db.check_config_dir() === false || await v_db.check_config_file() === false ) await v_db.install();
