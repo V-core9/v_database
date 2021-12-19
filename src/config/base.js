ebdf81fdd49a038c7ce258c583f94be2648585bb
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-19 12:47:54 +0100

    yea

--- src/config/base.js
+++ src/config/base.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const d_dir = '.v_database';
-const d_cfg = 'v_config';
+const d_cfg = 'config';
 const cfg_dpath = d_dir;
 const cfg_fpath = path.join(cfg_dpath,d_cfg);
 
