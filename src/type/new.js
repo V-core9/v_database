074496bbf9e192f43c23bfd80e132700feba0c0c
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-19 13:47:04 +0100

    yea

--- src/type/new.js
+++ src/type/new.js
@@ -1,5 +1,5 @@
 const v_fs = require('v_file_system');
 
 module.exports = async (type) => {
-  return (type !== undefined && type) ? await v_fs.mkdir(`${process.v.data_dir}/${type}`) : false;
+  return (type !== undefined ) ? await v_fs.mkdir(`${process.v.data_dir}/${type}`) : false;
 };
