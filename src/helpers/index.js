6275c0807e6642d2cf3b7805d368ea020e35be53
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-19 14:09:41 +0100

    yea missing init and version

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,8 @@
 module.exports = {
+  init: require('./init'),
   data_size: require('./data_size'),
   install: require('./install'),
   purge_database: require('./purge_database'),
   check_config_dir: require('./check_config_dir'),
-  check_config_file: require('./check_config_file'),
+  check_config_file: require('./check_config_file')
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const info = require('./info');
 const config  = require('./config');
-const { data_size, purge_database , install, check_config_dir, check_config_file } = require('./helpers');
+const { data_size, purge_database , install, check_config_dir, check_config_file , init} = require('./helpers');
 
 const v_database = {
+  init,
   info,
   config,
   data_size,
