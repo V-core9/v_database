00365a3592d53d44ce2d3308af1376aeb53c7c06
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-20 09:16:21 +0100

    yup yup some small adjustments

--- src/helpers/install/index.js
+++ src/helpers/install/index.js
@@ -2,25 +2,23 @@ const os = require('os');
 const v_fs = require("v_file_system");
 const inquirer = require('inquirer');
 const boxen = require('boxen');
-
 const { cfg_dpath, cfg_fpath } = require('../../config');
-
 
 module.exports = async () => {
   console.log(boxen(`This will now walk you through the setup process, should be easy to finish.`, { padding: 1, title: `V_Database Setup` }));
   const questions = require('./questions');
   inquirer.prompt(questions).then((answers) => {
 
-    //? Mark it
+    //? Installed System Information
     answers.installed = {
       timestamp: Date.now(),
-      //? System Architecture data
       os_arch: os.arch(),
       os_platform: os.platform(),
       os_version: os.version(),
       os_release: os.release(),
       os_type: os.type(),
     };
+
     //? Create the data folder
     v_fs.mkdirSy(cfg_dpath);
     //? Create the config file
