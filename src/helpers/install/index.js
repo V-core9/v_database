3a037ce307cf88a5025788668249cdff2c8d5bcf
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-20 09:14:19 +0100

    yea installation process cleanup

--- src/helpers/install/index.js
+++ src/helpers/install/index.js
@@ -7,19 +7,20 @@ const { cfg_dpath, cfg_fpath } = require('../../config');
 
 
 module.exports = async () => {
-  console.log(boxen(`This will now walk you through the setup process, should be easy to finish.`, { padding: 1, title: `V_Database Setup`}));
+  console.log(boxen(`This will now walk you through the setup process, should be easy to finish.`, { padding: 1, title: `V_Database Setup` }));
   const questions = require('./questions');
   inquirer.prompt(questions).then((answers) => {
 
     //? Mark it
-    answers.installed_ts = Date.now();
-    //? System Architecture data
-    answers.architecture = os.arch();
-    answers.installed_os_platform = os.platform();
-    answers.installed_os_version = os.version();
-    answers.installed_os_release = os.release();
-    answers.installed_os_type = os.type();
-
+    answers.installed = {
+      timestamp: Date.now(),
+      //? System Architecture data
+      os_arch: os.arch(),
+      os_platform: os.platform(),
+      os_version: os.version(),
+      os_release: os.release(),
+      os_type: os.type(),
+    };
     //? Create the data folder
     v_fs.mkdirSy(cfg_dpath);
     //? Create the config file
