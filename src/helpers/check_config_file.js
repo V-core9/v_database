f572e6db4d70c5fbe2313681f93bf4ea6c7c9812
unknown <slavko.vuletic92@gmail.com>
2021-12-31 06:24:34 +0100

    yea

--- src/helpers/check_config_file.js
+++ src/helpers/check_config_file.js
@@ -1,5 +1,5 @@
-const { cfg_fpath } = require('../config');
+const { root_dir, cfg_fpath } = require('../config');
 const v_fs = require('v_file_system');
 module.exports = async () => {
-  return (await v_fs.read(cfg_fpath+'.js') !== false) ? true : false;
+  return (await v_fs.read(root_dir+'/'+cfg_fpath+'.js') !== false) ? true : false;
 };
